236a27851bba87fea95a6572bd1f88a80de9fb51
Shani Trieu <shanibt@uci.edu>
2023-03-09 12:42:15 -0800

    Created sort_data() function to sort through all of the data given by the API to isolate the magnitudes of ONLY California's Earthquakes during the given date range

--- webapi.py
+++ webapi.py
@@ -6,10 +6,12 @@ import urllib.request
 import json
 
 def get_earthquake_data(start:str, end:str):
+    print('Opening and Reading Data....')
     url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + start + '&endtime=' + end
     req = urllib.request.Request(url)
     res = urllib.request.urlopen(req)
     res_data = res.read()
     res.close()
     api_data = json.loads(res_data)
+    print('Done!')
     return api_data
--- wp4.py
+++ wp4.py
@@ -5,14 +5,35 @@
 import webapi as WA
 import json
 
+def sort_data(data):
+    mag_list = []
+    loc_list = []
+
+    ind = 0
+    while ind != len(data['features']):
+        cali = data['features'][ind]['properties']['place']
+        if 'CA' in str(cali):
+            mag_list.append(data['features'][ind]['properties']['mag'])
+            loc_list.append(cali)
+        ind += 1
+    return mag_list
+
+
 
 if __name__ == "__main__":
-    start_date = input('Enter start date (YYYY-MM-DD): ')
-    end_date = input('Enter end date (YYYY-MM-DD): ')
-    filename = input('Filename to save Earthquake Data: ')
+    # start_date = input('Enter start date (YYYY-MM-DD): ')
+    # end_date = input('Enter end date (YYYY-MM-DD): ')
+    # filename = input('Filename to save Earthquake Data: ')
+    # visualname = input('Filename to save the graphical visual of the Earthquake Data: ')
 
-    earthquake = WA.get_earthquake_data(start_date, end_date)
+    start_date = '2023-01-09'
+    end_date = '2023-01-10'
+    filename = 'quake'
+
+    earthquake = WA.get_CAearthquake_data(start_date, end_date)
 
     out_file = open(filename, 'w')
     json.dump(earthquake, out_file)
     out_file.close
+
+    sort_data(earthquake)
